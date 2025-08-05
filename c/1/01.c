c1255b67b20d87609263d335b116c196d509c6ce
Kotone Kanno <160243360+kotonekanno@users.noreply.github.com>
2025-08-05 18:28:06 +0900

    Delete c/1/01.c

--- c/1/01.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-    printf(" /\\___/\\\t_\t _\n/\t\\  |_| ");
-    printf("|_ |  |  | |  /\n\\ * _ * /  |");
-    printf(" | |_ |_ |_ |_| *\n \\_____/\n");
-    return 0;
-}
